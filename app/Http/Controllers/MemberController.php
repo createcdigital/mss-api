f5ca9d5e09d45da54db694111bdcd120eaa8d101
qiaojun0312 <949042858@qq.com>
2017-04-21 16:13:08 +0800

    first commit

---
+++ app/Http/Controllers/MemberController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Member;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class MemberController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+//    update Member's Tag columns
+    public function updateMemberTag(Request $request)
+    {
+        $model =Member::find($request->id);
+
+        $model->tag = $request->tag;
+
+        if($model->save())
+            return json_encode(["rs" => "success"]);
+        else
+            return json_encode(["rs" => "error"]);
+    }
+}
---
+++ app/Http/Controllers/MemberTagLogController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Member_tag_log;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class MemberTagLogController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+//    Add Member and Tag Relationship Update log
+    public function add(Request $request)
+    {
+        $model = new Member_tag_log();
+        $model->member_wechat_openid = $request->member_wechat_openid;
+        $model->before_edit_tagid = $request->before_edit_tagid;
+        $model->after_edit_tagid = $request->after_edit_tagid;
+
+        if($model->save())
+            return json_encode(["rs" => "success"]);
+        else
+            return json_encode(["rs" => "error"]);
+    }
+}
